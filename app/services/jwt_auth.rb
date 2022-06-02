9e749f359e95d5f5f4248b5f0ae4cfc95bac69c6
ginohmk <michotall95@gmail.com>
2022-06-02 14:49:07 +0100

    Add jwt decoding

--- app/services/jwt_auth.rb
+++ app/services/jwt_auth.rb
@@ -3,4 +3,8 @@ class JwtAuthService
   def self.encode(payload)
     JWT.encode payload, HMAC_SECRET_KEY, 'HS256'
   end
+
+  def self.decode(token)
+    JWT.decode(token, HMAC_SECRET_KEY, true, { algorithm: 'HS256' })
+  end 
 end
