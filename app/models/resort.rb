d46455eb65d6a18637c5b0d12697d3c90c2f5463
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-06-02 13:22:21 +0300

    Added relationships resort, user to reservation

--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,4 +1,6 @@
 class Resort < ApplicationRecord
+  has_many :reservations
+  has_many :users, through: :reservations
   validates :name, presence: true
   validates :city, presence: true
   validates :country, presence: true
