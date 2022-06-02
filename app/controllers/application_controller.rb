f924791d7d0683a51dc64a7d1a5e611ee7b4c4da
ginohmk <michotall95@gmail.com>
2022-06-02 15:33:01 +0100

    Add authenticate user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def authenticate_user
+    unless request.headers['Authorization'].present?
+      render json: { message: 'Authorization token missing' }, status: :unprocessable_entity and return
+    end
+    headers = request.headers['Authorization']
+    decoded = JsonWebToken.decode headers
+  end
 end
