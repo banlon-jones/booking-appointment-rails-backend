974f7a0b66b8fdde17dad600bc2f948a7bd9569b
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-06-02 13:15:45 +0300

    Created reservation commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_094046) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_02_101259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "date_from"
+    t.datetime "date_to"
+    t.bigint "user_id", null: false
+    t.bigint "resort_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["resort_id"], name: "index_reservations_on_resort_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "resorts", force: :cascade do |t|
     t.string "name", null: false
@@ -32,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_094046) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "resorts"
+  add_foreign_key "reservations", "users"
 end
