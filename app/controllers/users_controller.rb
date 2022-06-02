9a367fdf0aad166f021cd53b0af246f6468699c0
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-06-02 18:21:54 +0300

    Added signup method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,26 @@ class UsersController < ApplicationController
       render json: { error: 'Invalid login details' }, status: :unauthorized
     end 
   end
+
+  def signup
+    if (params[:name].present? &&
+       params[:email].present? &&
+       params[:password].present? &&
+       params[:password_confirmation].present?)
+       if params[:password] == params[:password_confirmation]
+        @user = User.create(allowed_params)
+        render json: { success: 'Successfully signed up, you can now login' }, status: :created
+       else
+        render json: { error: 'Password missmatch' }, status: :conflict
+      end
+    else
+      render json: { error: 'missing signup details' }, status: :partial_content
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.require( :user ).permit( :name, :email, :password, :password_confirmation)
+  end
 end
