28789e3e3fc3ccbde654b242f8669d08383acf84
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-06-03 15:17:18 +0300

    Added tests for the reservation controller

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -1,7 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe "Reservations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  reservation = Reservation.new(
+    resort_id: 'resort.id',
+    date_from: '2022-07-09',
+    date_to: '2022-07-15'
+  )
+
+  describe 'GET /index' do
+    before(:example) { get '/reservations' }
+
+    it 'should returns http 422 success' do
+      expect(response).to have_http_status(422)
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get '/reservations' }
+
+    it 'should returns http 422 success' do
+      expect(response).to have_http_status(422)
+    end
+  end
+
+  describe 'Post /create' do
+    before(:example) do
+      post '/reservations', params: {
+        reservation: {
+          resort_id: '1',
+          date_from: '2022-07-09',
+          date_to: '2022-07-15'
+        }
+      }
+    end
+
+    it 'should returns http 422 created' do
+      expect(response).to have_http_status(422)
+    end
   end
 end
