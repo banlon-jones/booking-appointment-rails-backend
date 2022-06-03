7bccd73aedb833824c90a1ec04ad704718a9fea1
Vana Seraydarian <vanaseraydarian123@gmail.com>
2022-06-03 15:44:50 +0300

    Added as_json methods

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,13 @@ class Reservation < ApplicationRecord
   belongs_to :resort
   validates :date_from, presence: true
   validates :date_to, presence: true
+
+  def as_json(options = {})
+    {
+      :id => self.id,
+      :date_from => self.date_from,
+      :date_to => self.date_to,
+      :resort => self.resort
+    }
+  end
 end
--- app/models/resort.rb
+++ app/models/resort.rb
@@ -7,4 +7,16 @@ class Resort < ApplicationRecord
   validates :image, presence: true
   validates :description, presence: true
   validates :cost, presence: true, comparison: { greater_than: 0.0 }, numericality: true
+
+  def as_json(options = {})
+    {
+      :id => self.id,
+      :name => self.name,
+      :city => self.city,
+      :country => self.country,
+      :image => self.image,
+      :description => self.description,
+      :cost => self.cost
+    }
+  end
 end
