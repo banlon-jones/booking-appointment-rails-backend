792616db6c30f877e6fcdd6fe7a012a8fd0c2f71
ginohmk <michotall95@gmail.com>
2022-06-07 15:00:21 +0100

    Clean up reservation documentation code

--- spec/requests/reservation_swagger_spec.rb
+++ spec/requests/reservation_swagger_spec.rb
@@ -2,12 +2,9 @@ require 'swagger_helper'
 require 'rails_helper'
 
 describe 'Resorts API' do
-  let(:access_token) { 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MX0.TEtXvT29xph6s8cOeb5O3ko1cNj6gA6alr7ZOy4RCg0' }
-  let(:Authorization) { "Bearer #{access_token}" }
-
   # Index
   path '/reservations' do
-    get 'Retrieves a reservation' do
+    get 'Retrieves reservations' do
       tags 'Reservations'
       produces 'application/json'
       security [Bearer: {}]
@@ -22,16 +19,6 @@ describe 'Resorts API' do
                  resort_id: { type: :integer }
                },
                required: %w[id date_from date_to user_id resort_id]
-        run_test!
-      end
-
-      response '404', 'Reservations not found' do
-        let(:id) { 'invalid' }
-        run_test!
-      end
-
-      response '406', 'unsupported accept header' do
-        let(:Accept) { 'application/foo' }
         run_test!
       end
     end
@@ -54,12 +41,6 @@ describe 'Resorts API' do
       }
 
       response '201', 'reservation created' do
-        let(:reservation) { { date_from: '2022-10-4', date_to: '2022-12-4', user_id: 1, resort_id: 1} }
-        run_test!
-      end
-
-      response '422', 'invalid request' do
-        let(:reservation) { { name: 'foo' } }
         run_test!
       end
     end
