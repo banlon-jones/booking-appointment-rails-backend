3f4c6813143b17a2466788a73a55abe60fe6ae61
ginohmk <michotall95@gmail.com>
2022-06-07 14:58:58 +0100

    Add resort#create and update  action  end point documentation

--- spec/requests/resort_swagger_spec.rb
+++ spec/requests/resort_swagger_spec.rb
@@ -65,5 +65,58 @@ describe 'Resorts API' do
     end
   end
 
-  
+  # Create
+  path '/resorts' do
+    post 'Creates a resort' do
+      tags 'Resorts'
+      consumes 'application/json'
+      security [Bearer: {}]
+      parameter name: :resort, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          city: { type: :string },
+          country: { type: :string },
+          image: { type: :string },
+          description: { type: :string },
+          cost: { type: :number },
+          isDeleted: { type: :boolean }
+        },
+        required: %w[name city country image cost isDeleted]
+      }
+
+      response '201', 'resort created' do
+        run_test!
+      end
+    end
+  end
+
+  # Update
+  path '/resorts/{id}' do
+    patch 'Update a resort' do
+      tags 'Resorts'
+      consumes 'application/json'
+      parameter name: :id, in: :path, type: :integer
+      security [Bearer: {}]
+      parameter name: :resort, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          city: { type: :string },
+          country: { type: :string },
+          image: { type: :string },
+          description: { type: :string },
+          cost: { type: :number },
+          isDeleted: { type: :boolean }
+        },
+        required: %w[name city country image cost isDeleted]
+      }
+
+      response '200', 'resort Updated' do
+        run_test!
+      end
+    end
+  end
+
+ 
 end
